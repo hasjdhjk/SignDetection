0b4f83f7839025a841a9ca313b9decc45a6a2f04
Lok <lokman.chan23@imperial.ac.uk>
2025-01-25 13:42:05 +0000

    initialisedvideo

---
+++ mediapipe/video_processor.py
@@ -0,0 +1,51 @@
+import cv2
+from mediapipe.hand_tracker import HandTracker
+import numpy as np
+
+#---------------------------------------------#
+# INITIALISE CAMERA                           #
+# first function called                       #
+#---------------------------------------------#
+def initialize_video(file_name):
+    video = cv2.VideoCapture(file_name)
+    if not video.isOpened():
+        print("Error: Could not open video file.")
+        exit()
+    return video
+
+
+#---------------------------------------------#
+# MAIN                                        #
+#                                             #
+#---------------------------------------------#
+def main():
+    video = initialize_video('name_of_file')
+    hand_tracker = HandTracker()
+    # VIDEO = a function that loads video. 
+    # In real_time_overlay, the class type of 'cap' is <class 'cv2.VideoCapture'>. 
+
+    # Error handling for video processing
+    while True:
+        success, frame = video.read()
+        if not success:
+            print("Error: Video loading failed.")
+            break
+
+        # Process the frame and get hand landmarks
+        processed_frame, results = hand_tracker.process_frame(frame)
+
+        # Print hand landmarks
+        hand_tracker.print_hand_landmarks(results)
+
+        # Draw hand landmarks on the frame
+        hand_tracker.draw_landmarks(processed_frame, results)
+
+        # Draw handedness on the frame
+        hand_tracker.draw_handedness(processed_frame, results)
+
+        # Display the annotated frame
+        cv2.imshow("Hand Tracking", processed_frame)
+
+
+if __name__ == "__main__":
+    main()
